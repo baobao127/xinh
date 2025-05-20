cafaa1f29465f087a532d65c28a7ee42b334e3c3
baobao017 <mna79001@gmail.com>
2025-05-20 08:00:34 +0700

    Update SkeletonProduct.tsx

--- src/component/SkeletonProduct.tsx
+++ src/component/SkeletonProduct.tsx
@@ -1,7 +1,7 @@
 const SkeletonProduct = () => {
   return (
-    <div className="animate-pulse p-4 border rounded">
-      <div className="h-40 bg-gray-200 mb-4 rounded" />
+    <div className="animate-pulse p-2 border rounded">
+      <div className="h-32 bg-gray-200 mb-3 rounded" />
       <div className="h-4 bg-gray-300 mb-2 rounded w-3/4" />
       <div className="h-4 bg-gray-300 rounded w-1/2" />
     </div>
