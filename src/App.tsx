1ae1ea3cdcbeb8f6f82cd71896311f04de6e113d
baobao017 <mna79001@gmail.com>
2025-05-20 19:29:31 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import AppRoutes from './AppRoutes';
 import { useToast } from '@/hooks/use-toast';
+import MiniGameCoupon from '@/components/MiniGameCoupon';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
+import FakePurchasePopup from '@/components/FakePurchasePopup';
+
+<Layout>
+  <MiniGameCoupon />
+  <ScrollToTopButton />
+  <FakePurchasePopup />
+  {/* ... */}
+</Layout>
 
 function App() {
   const { Toast } = useToast();
