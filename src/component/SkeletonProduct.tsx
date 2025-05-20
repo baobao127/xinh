c091dfb68dff409ca2b87b8ac7022cdea6256502
baobao017 <mna79001@gmail.com>
2025-05-20 07:52:03 +0700

    Create SkeletonProduct.tsx

---
+++ src/component/SkeletonProduct.tsx
@@ -0,0 +1,11 @@
+const SkeletonProduct = () => {
+  return (
+    <div className="animate-pulse p-4 border rounded">
+      <div className="h-40 bg-gray-200 mb-4 rounded" />
+      <div className="h-4 bg-gray-300 mb-2 rounded w-3/4" />
+      <div className="h-4 bg-gray-300 rounded w-1/2" />
+    </div>
+  );
+};
+
+export default SkeletonProduct;
