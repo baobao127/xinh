6b238e0e4ec51bcb69b8355ec7147b536ceaa787
baobao017 <mna79001@gmail.com>
2025-05-20 09:04:46 +0700

    Create WishlistContext.tsx

---
+++ src/context/WishlistContext.tsx
@@ -0,0 +1,36 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const WishlistContext = createContext<any>(null);
+
+export const WishlistProvider = ({ children }: { children: React.ReactNode }) => {
+  const [wishlist, setWishlist] = useState<any[]>([]);
+
+  useEffect(() => {
+    const stored = localStorage.getItem('wishlist');
+    if (stored) setWishlist(JSON.parse(stored));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+  }, [wishlist]);
+
+  const toggleWishlist = (product: any) => {
+    const exists = wishlist.find((p) => p.id === product.id);
+    if (exists) {
+      setWishlist(wishlist.filter((p) => p.id !== product.id));
+    } else {
+      setWishlist([...wishlist, product]);
+    }
+  };
+
+  const isInWishlist = (id: string | number) =>
+    wishlist.some((p) => p.id === id);
+
+  return (
+    <WishlistContext.Provider value={{ wishlist, toggleWishlist, isInWishlist }}>
+      {children}
+    </WishlistContext.Provider>
+  );
+};
+
+export const useWishlist = () => useContext(WishlistContext);
