e1b2c5952de78987167d2f20398d6be2a4fd59d6
baobao127 <bechauonmyojituusp@gmail.com>
2025-05-20 20:01:04 +0700

    Create Productlist.tsx

---
+++ src/component/Productlist.tsx
@@ -0,0 +1,9 @@
+const [view, setView] = useState('grid');
+
+<ViewToggle view={view} setView={setView} />
+
+<div className={view === 'grid' ? 'grid grid-cols-2 gap-4' : 'space-y-4'}>
+  {products.map(p => (
+    <div className="border p-2">{p.name}</div>
+  ))}
+</div>
