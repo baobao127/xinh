36d293d77fd3affaa8051f8c2c04bef9bd9ad5f5
baobao017 <mna79001@gmail.com>
2025-05-21 10:43:05 +0700

    Update SkeletonProduct.tsx

--- src/component/SkeletonProduct.tsx
+++ src/component/SkeletonProduct.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
 
-const SkeletonProduct = () => (
-  <div className="animate-pulse p-2 border rounded space-y-2">
-    <div className="bg-gray-300 h-32 rounded" />
-    <div className="bg-gray-300 h-4 w-3/4 rounded" />
-    <div className="bg-gray-300 h-4 w-1/2 rounded" />
-  </div>
-);
-
-
 const SkeletonProduct = () => {
   return (
     <div className="animate-pulse p-2 border rounded">
