6ffcb0d4ea1ca649a0bf21d946da4f2c909d0224
baobao017 <mna79001@gmail.com>
2025-05-22 09:23:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import MiniGameCoupon from '@/components/MiniGameCoupon';
 import ScrollToTopButton from '@/components/ScrollToTopButton';
 import FakePurchasePopup from '@/components/FakePurchasePopup';
 import { Toaster } from 'react-hot-toast';
+import DiscountPopup from '@/components/DiscountPopup';
 
+const [adminUnlocked, setAdminUnlocked] = useState(false);
+
+<DiscountPopup onUnlockAdmin={() => setAdminUnlocked(true)} />
+
+{adminUnlocked && (
+  <Link to="/admin/orders" className="text-blue-600 underline mt-2 block">Vào trang Admin</Link>
+)}
 function App() {
   return (
     <>
